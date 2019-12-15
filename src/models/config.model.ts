adc94392f1c038950c08ef0469c0197eadc4d691
Aymen Elmehdhaoui <aymen.el.mehdhaoui@ced.group>
2019-12-15 02:14:29 +0100

    delete local config + files

--- src/models/config.model.ts
+++ src/models/config.model.ts
@@ -1,4 +1,4 @@
 export class Config {
-    public filePath: string = 'C:\\Users\\CED 30\\Desktop\\code\\generate-swagger\\swagger.json';
-    public outDir: string = './output';
+    public filePath: string = '';
+    public outDir: string = '';
 }
